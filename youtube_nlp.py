2c1291f7302c7bc5d10abe86890f2a979bd940cc
Sandhya Rattnam <ratna031@umn.edu>
2020-08-23 12:17:21 -0500

    youtube transcript api works with lists

--- youtube_nlp.py
+++ youtube_nlp.py
@@ -1,10 +1,14 @@
 import sys
 from youtube_transcript_api import YouTubeTranscriptApi
 
-video_ids = "mkF7Ep6gygQ"
-script = YouTubeTranscriptApi.get_transcript(video_ids)
+video_ids = ["mkF7Ep6gygQ", "mkF7Ep6gygQ"]
+script = []
+for i in range(len(video_ids)):
+    script.append(YouTubeTranscriptApi.get_transcript(video_ids[i]))
 print(script)
 
 with open('sample_caption.txt',"w") as filehandle:
-    for listitem in script:
-        filehandle.write(listitem.get('text')+" ")
+    for i in range(len(script)):
+        for listitem in script[i]:
+            filehandle.write(listitem.get('text')+" ")
+
